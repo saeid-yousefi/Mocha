6edb9e74bf29067f112943995176ec3d0d7e13d0
saeid.yousefi.1373@gmail.com <saeid.yousefi.1373@gmail.com>
2024-06-25 12:27:11 +0330

    add strings text

--- mocha/src/test/java/ir/sy/mocha/data_creators/CreateStringTest.kt
+++ mocha/src/test/java/ir/sy/mocha/data_creators/CreateStringTest.kt
@@ -1,66 +1,53 @@
 package ir.sy.mocha.data_creators
 
-import android.content.Context
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldBeIn
 import io.kotest.matchers.shouldBe
-import io.mockk.every
-import io.mockk.mockk
 import ir.sy.mocha.mocker.annotations.MockString
 import ir.sy.mocha.mocker.data_creators.createString
-import ir.sy.mocha.mocker.data_creators.getStringArrayFromResources
 import ir.sy.mocha.mocker.types.StringType
+import ir.sy.mocha.resources.EnglishResources
+import ir.sy.mocha.resources.Languages
 
 class CreateStringTest : BehaviorSpec({
-    val context = mockk<Context>(relaxed = true)
-
+    val language = Languages.English
     Given("valid variable name without annotations") {
         `when`("create data for Name variable") {
-            val names = arrayOf("john", "smith")
-            every { getStringArrayFromResources(context, "mocha_name") } returns names
             val variableName = "firstname"
-            val result = createString(variableName, context)
+            val result = createString(variableName, language)
             then("result should be name") {
-                result shouldBeIn names
+                result shouldBeIn EnglishResources.name
             }
         }
         `when`("create data for Country") {
-            val countries = arrayOf("country1", "country2")
-            every { getStringArrayFromResources(context, "mocha_country") } returns countries
             val variableName = "user_nationality"
-            val result = createString(variableName, context)
+            val result = createString(variableName, language)
             then("result should be in countries") {
-                result shouldBeIn countries
+                result shouldBeIn EnglishResources.country
             }
         }
         `when`("create data for undefined variable") {
-            val items = arrayOf("item1", "item2")
-            every { getStringArrayFromResources(context, "mocha_undefined") } returns items
             val variableName = "aa_bb_cc"
-            val result = createString(variableName, context)
+            val result = createString(variableName, language)
             then("result should be in countries") {
-                result shouldBeIn items
+                result shouldBeIn EnglishResources.undefined
             }
         }
     }
     Given("Variable name with Annotation") {
         val variableName = "sth"
         `when`("email annotation") {
-            val emails = arrayOf("email@email.com")
-            every { getStringArrayFromResources(context, "mocha_email") } returns emails
             val annotation = MockString(type = StringType.Email)
-            val result = createString(variableName, context, annotation)
+            val result = createString(variableName, language, annotation)
             then("result should be email") {
-                result shouldBeIn emails
+                result shouldBeIn EnglishResources.email
             }
         }
         `when`("custom annotation with word count") {
-            val items = arrayOf("The quick brown")
-            every { getStringArrayFromResources(context, "mocha_custom") } returns items
             val annotation = MockString(type = StringType.Custom, wordCount = 1)
-            val result = createString(variableName, context, annotation)
+            val result = createString(variableName, language, annotation)
             then("result should be The") {
-                result shouldBe "The"
+                result.split(" ").size shouldBe 1
             }
         }
     }
